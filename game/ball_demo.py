827154a44204cd9860ca30a80eda6b240dc46f89
Amelie02 <77777319+Amelie02@users.noreply.github.com>
2021-01-21 15:16:24 +0100

    add rectangle

--- game/ball_demo.py
+++ game/ball_demo.py
@@ -15,10 +15,25 @@ pygame.init()
 screen = pygame.display.set_mode(size)
 running = True
 
+ball = pygame.image.load('ball.gif')
+rect = ball.get_rect()
+speed = [2, 2]
+
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+            
+        rect = rect.move(speed)
+        if rect.left < 0 or rect.right > width:
+            speed[0] = - speed[0]
+        if rect.top < 0 or rect.bottom > height:
+            speed[1] = - speed[1]
+        
+    screen.fill(GREEN)
+    pygame.draw.rect(screen, RED, rect, 1)
+    screen.blit(ball, rect)
+    pygame.display.update()
             
 pygame.quit()
             
---
+++ game/rectangle.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+# size = 640, 320
+# width, height = size
+
+width = 640
+height = 320
+
+size = (width,height)
+GREEN = (150, 255, 255)
+RED = (255, 0, 0)
+GRAY = (127, 127, 127)
+background = GRAY
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+screen.fill(background)
+
+pygame.draw.rect(screen, RED, (50, 120 220, 100))
+pygame.draw.rect(screen, GREEN, (150, 150, 220,
+
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+running = True
