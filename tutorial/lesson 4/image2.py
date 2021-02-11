16e3d0e111bc39ed2b8474585a911d4a0e1b089b
Amelie02 <77777319+Amelie02@users.noreply.github.com>
2021-02-11 15:18:43 +0100

    add files

--- tutorial/lesson 4/image2.py
+++ tutorial/lesson 4/image2.py
@@ -4,9 +4,14 @@ from pygame.locals import *
 SIZE = 500, 500
 RED = (255, 0, 0)
 GRAY = (150, 150, 150)
+GREEN = (0, 255, 0)
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
+path = 'bird.png'
+
+img = pygame.image.load(path)
+img.convert()
 
 img0 = pygame.image.load(path)
 img0.convert()
@@ -14,7 +19,40 @@ img0.convert()
 rect0 = img0.get_rect()
 pygame.draw.rect(img0, GREEN, rect0, 1)
 
-center = 250, 250
-img = img0
 rect = img.get_rect()
-rect.center = center
+rect.center = 250, 250
+moving = False
+
+angle = 0
+scale = 1
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+        elif event.type == KEYDOWN:
+            if event.key == K_r:
+                if event.mod & KMOD_SHIFT:
+                    angle -= 10
+                else:
+                    angle += 10
+                
+                img = pygame.transform.rotozoom(img0, angle, scale)
+    
+        elif event.type == MOUSEBUTTONDOWN:
+            moving = True
+            
+        elif event.type == MOUSEBUTTONUP:
+            moving = False
+            
+        elif event.type == MOUSEMOTION and moving:
+            rect.move_ip(event.rel)
+            
+    screen.fill(GRAY)
+    pygamedraw.rect(screen,RED,rect, 2)
+    screen.bilt(img, rect)
+    pygame.display.flip()
+    
+pygame.quit
